2ddf74cd4ea1e6e3ed13a9ddc85b7463a54eb8ba
Merve Balci <mervebalci090@gmail.com>
2024-01-01 15:32:41 -0400

    Functioning the decimal point button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { CalcContext } from "../context/CalcContext";
+
 function getStyleName(btn) {
   const className = {
     "=": "equals",
@@ -11,8 +14,23 @@ function getStyleName(btn) {
 }
 
 export default function Button({ value }) {
+  const { calc, setCalc } = useContext(CalcContext);
+
+  function handleButtonClick() {
+    const results = {
+      ".": decimalClick,
+    }
+    return results[value]()
+  }
+
+
+  // When user clicks on decimal point "." button
+  function decimalClick() {
+    setCalc({...calc, number: !calc.number.toString().includes('.') ? calc.number + value : calc.number})
+  }
+
   return (
-    <button className={`${getStyleName(value)} button`}>
+    <button onClick={handleButtonClick} className={`${getStyleName(value)} button`}>
       {value}
     </button>
   )
